592a3159b62ae8e6e3eeeec74c4e74e19d84beeb
Vitor Mignoni <vichsort@proton.me>
2024-12-02 22:20:18 -0300

    ASIDE + MAIN

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
-  <appNav msg="This Should be a Nav..."/>
-  <appImages msg="Just a little bit!"/>
-  <appCards msg="A lot, by the way!"/>
+  <appNav />
+  <div class="outer-container">
+    <aside> <!-- CARDS -->
+      <appCards msg="A lot, by the way!"/>
+    </aside> 
+
+
+
+    <main> <!-- IMAGES -->
+      <appImages msg="Just a little bit!"/>
+    </main>
+  </div>
+
+
   <appFooter msg="And this should be a footer..."/>
 </template>
 
@@ -33,5 +44,29 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.outer-container {
+  display: flex;
+}
+
+aside, main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: white
+}
+
+aside {
+  background-color: #3a3de3;
+  width: 30%;
+  min-height: 100dvh;
+}
+
+main {
+  background-color: #e33a3a;
+  width: 70%;
+  min-height: 100dvh;
 }
 </style>
