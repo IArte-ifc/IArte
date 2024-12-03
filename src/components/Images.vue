eb5febcc15c9dc1efcdcd7d6f457de94c29ae633
Vitor Mignoni <vichsort@proton.me>
2024-12-02 22:30:18 -0300

    images start

--- src/components/Images.vue
+++ src/components/Images.vue
@@ -1,6 +1,44 @@
 <template>
-  <h1>ImageSection Works!</h1>
-  <p>{{ msg }}</p>
+  <div class="self">
+    <img src="../assets/1.png" alt="Imagem gerada por IA">
+    <img src="../assets/2.png" alt="Imagem gerada por IA">
+    <img src="../assets/3.png" alt="Imagem gerada por IA">
+    <img src="../assets/4.png" alt="Imagem gerada por IA">
+    <img src="../assets/5.png" alt="Imagem gerada por IA">
+    <img src="../assets/6.png" alt="Imagem gerada por IA">
+    <img src="../assets/7.png" alt="Imagem gerada por IA">
+    <img src="../assets/8.png" alt="Imagem gerada por IA">
+    <img src="../assets/9.png" alt="Imagem gerada por IA">
+    <img src="../assets/10.png" alt="Imagem gerada por IA">
+    <img src="../assets/11.png" alt="Imagem gerada por IA">
+    <img src="../assets/12.png" alt="Imagem gerada por IA">
+    <img src="../assets/13.png" alt="Imagem gerada por IA">
+    <img src="../assets/14.png" alt="Imagem gerada por IA">
+    <img src="../assets/15.png" alt="Imagem gerada por IA">
+    <img src="../assets/16.png" alt="Imagem gerada por IA">
+    <img src="../assets/17.png" alt="Imagem gerada por IA">
+    <img src="../assets/18.png" alt="Imagem gerada por IA">
+    <img src="../assets/19.png" alt="Imagem gerada por IA">
+    <img src="../assets/20.png" alt="Imagem gerada por IA">
+    <img src="../assets/21.png" alt="Imagem gerada por IA">
+    <img src="../assets/22.png" alt="Imagem gerada por IA">
+    <img src="../assets/23.png" alt="Imagem gerada por IA">
+    <img src="../assets/24.png" alt="Imagem gerada por IA">
+    <img src="../assets/25.png" alt="Imagem gerada por IA">
+    <img src="../assets/26.png" alt="Imagem gerada por IA">
+    <img src="../assets/27.png" alt="Imagem gerada por IA">
+    <img src="../assets/28.png" alt="Imagem gerada por IA">
+    <img src="../assets/29.png" alt="Imagem gerada por IA">
+    <img src="../assets/30.png" alt="Imagem gerada por IA">
+    <img src="../assets/31.png" alt="Imagem gerada por IA">
+    <img src="../assets/32.png" alt="Imagem gerada por IA">
+    <img src="../assets/33.png" alt="Imagem gerada por IA">
+    <img src="../assets/34.png" alt="Imagem gerada por IA">
+    <!-- <img src="../assets/35.png" alt="Imagem gerada por IA">
+    <img src="../assets/36.png" alt="Imagem gerada por IA"> -->
+    <img src="../assets/37.png" alt="Imagem gerada por IA">
+  </div>
+
 </template>
 
 <script>
@@ -13,18 +51,11 @@ export default {
 </script>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.self {
+  padding: 7rem 0;
+
+  img {
+    width: 39%;
+  }
 }
 </style>
